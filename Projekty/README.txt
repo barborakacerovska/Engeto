52f46b5fe0156c4522661c2f2c7f2697af402442
Barbora Kacerovska <barbora.kacerovska@gmail.com>
2020-12-29 17:04:12 +0100

    Doděláno life_expectancy, funguje religions Religions jde napsat lepe, musim to overit. Zbyva dodelat weather.

--- Projekty/README.txt
+++ Projekty/README.txt
@@ -1,12 +1,32 @@
-Tabulka obsahuje časově závislé hodnoty nakažených/vylěčených/úmrtí pro každou zemi včetně požadovaných charakteristik
-pro dané země (viz níže).
+﻿Tabulka obsahuje časově závislé hodnoty nakažených/vylěčených/úmrtí pro každou zemi včetně požadovaných charakteristik
+ pro dané země (viz níže).
+
+
 
 Hodnoty pro sloupce mortality_under5, GDP_population a gini odpovídají hodnotám pro rok 2018.
 
 
-### Popis sloupců
-* WEEKEND: Ke každému datu je přiřazena binární hodnota pokud jde o pracovní den (0) nebo víkend (1).
-* SEASON: 0-zimní měsíce, 1 - jarní měsíce, 2 - letní měsíce, 3 - podzimní měsíce
-* POP_DENSITY_CALCULTED: vypočtená hustota zalidnění
-* GDP_POPULATION: HDP na obyvatele
 
+==============
+
+
+
+Popis sloupců
+
+==============
+
+* WEEKEND: Ke každému datu je přiřazena binární hodnota pokud jde o pracovní den (0) nebo víkend (1).
+
+* SEASON: 0-zimní měsíce, 1 - jarní měsíce, 2 - letní měsíce, 3 - podzimní měsíce
+
+* POPULATION: celková populace dané země
+* POP_DENSITY_CALCULTED: vypočtená hustota zalidnění
+
+* GDP_POPULATION: HDP na obyvatele, vypočteno podle údajů z roku 2018
+* GINI: gini koeficient (pro rok 2018)
+* MEDIAN_AGE_2018: medián věku obyvatel v roce 2018
+* REGION_IN_WORLD: světový region dané země
+* LIFE_EXP_DIFF: rozdíl mezi očekávanou dobou dožití v roce 1965 a v roce 2015
+
+
+náboženství rok 2020
--- Projekty/SQL_projekt.sql
+++ Projekty/SQL_projekt.sql
@@ -1,48 +1,27 @@
 SELECT 
-	cb.country,
-	cb.date,
+	cb.*,
 	case when weekday(date) in (5,6) then 1 else 0 end as weekend,  # 5=sobota,  6=nedele
 	case when month(date) in (12, 1, 2) then 0
       when month(date) in (3, 4, 5) then 1
       when month(date) in (6, 7, 8) then 2
       when month(date) in (9, 10, 11) then 3 # mesice
 	  end as season,
-	cb.confirmed,
-	cb.recovered,
-	cb.deaths,
-	lt.code3 ,
-	lt.iso3,
-	lt.lat,
-	lt.`long` ,
 	lt.population,
-	c.avg_height ,
-	c.continent ,
-	c.religion ,
-	c.elevation ,
-	c.government_type ,
-	c.independence_date ,
-	c.landlocked ,
-	c.life_expectancy ,
-	c.north ,
-	c.east ,
-	c.west ,
-	c.south ,
-	c.population_density ,
 	ROUND(c.population/c.surface_area,3) as pop_density_calculated,
-	c.region_in_world ,
-	c.surface_area ,
-	c.yearly_average_temperature ,
-	c.median_age_2018,
+	e.GDP_population, 
 	e.gini,
-	e.mortaliy_under5 ,
-	e.GDP_population
-FROM (select * from covid19_basic cb where date='2020-03-03') cb   #doèasnì
-LEFT JOIN (   							#pridavam informace o statech
-	select * 
+	e.mortaliy_under5,
+	c.median_age_2018,
+	c.region_in_world,
+	le.life_exp_diff
+FROM (select * from covid19_basic cb where date='2020-03-03') cb   #doèasnì, **********ZRUŠIT
+LEFT JOIN (   							
+	select population, country, iso3 
 	from lookup_table lt
 	where province is null ) lt
 	on cb.country = lt.country
-LEFT JOIN (select *
+LEFT JOIN (
+	select iso3, median_age_2018, region_in_world, population, surface_area
 	from countries c 
 	where iso3 is not null) c 
 	on lt.iso3 = c.iso3 
@@ -50,26 +29,48 @@ LEFT JOIN (
 	select country, gini, mortaliy_under5, round(GDP/population ) as GDP_population
 	from economies e 
 	where year = 2018) e
-	on cb.country = e.country;   #Vyøešit èesko a další zemì
+	on cb.country = e.country   #Vyøešit èesko a další zemì
+LEFT JOIN (
+	select le1.iso3, round(le1.life_exp_2015-le2.life_exp_1965,2) as life_exp_diff
+	from (
+		select iso3, life_expectancy as 'life_exp_2015'  from life_expectancy le 
+		where year = 2015) le1
+	left join (
+		select iso3, life_expectancy as 'life_exp_1965' from life_expectancy le 
+		where year = 1965) le2
+		on le1.iso3 = le2.iso3
+	where le1.iso3 is not null) le
+	on lt.iso3 = le.iso3;
 
 
 
-select * 
+	
+
+select r4.country, sum(r4.Christianity) as 'Christianity', sum(r4.Islam) as Islam, sum(r4.Hinduism) as 'Hinduism' ,
+	sum(r4.`Unaffiliated Religions`) as 'Unaffiliated Religions', sum(r4.Buddhism) as 'Buddhism', sum(r4.`Folk Religions`) as 'Folk Regions',
+	sum(r4.`Judaism`) as 'Judaism', sum(r4.`Other Religions`) as 'Other Religions'
 from (
-	select country,population, GDP, gini, fertility, mortaliy_under5, `index` as economy_index, round(GDP/population ) as GDP_population
-	from economies e 
-	where year = 2018) e;   # data za rok 2018!
+select r2.country, 
+	case when r3.religion='Christianity'  then round(r3.population/r2.pop_total*100,2)  else 0 end as 'Christianity',
+	case when r3.religion='Islam'  then round(r3.population/r2.pop_total*100,2)  else 0 end as 'Islam',
+	case when r3.religion='Hinduism'  then round(r3.population/r2.pop_total*100,2)  else 0 end as 'Hinduism',
+	case when r3.religion='Unaffiliated Religions'  then round(r3.population/r2.pop_total*100,2)  else 0 end as 'Unaffiliated Religions',
+	case when r3.religion='Buddhism'  then round(r3.population/r2.pop_total*100,2)  else 0 end as 'Buddhism',
+	case when r3.religion='Folk religions'  then round(r3.population/r2.pop_total*100,2)  else 0 end as 'Folk religions',
+	case when r3.religion='Judaism'  then round(r3.population/r2.pop_total*100,2)  else 0 end as 'Judaism',
+	case when r3.religion='Other Religions'  then round(r3.population/r2.pop_total*100,2)  else 0 end as 'Other Religions'
+from (
+	select country, sum(population) as pop_total
+	from religions r2
+	where year=2020
+	group by country) r2
+left join (
+	select religion, country, population
+	from religions r3 
+	where year = 2020) r3
+	on r2.country=r3.country) r4
+group by r4.country ;
 
-SELECT * 	
-FROM (
-select iso3,life_expectancy 
-from life_expectancy le 
-where year = 2018) le;
-	
-	
-select *
-from countries c2 ;
 
-select *
-from lookup_table lt ;
+
  
