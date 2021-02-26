bd42d15fcedd558057bc7473becc794df98d939b
Barbora Kacerovska <barbora.kacerovska@gmail.com>
2021-02-26 11:31:35 +0100

    finalni verze

--- Projekty/README.txt
+++ Projekty/README.txt
@@ -1,9 +1,11 @@
-﻿Tabulka obsahuje časově závislé hodnoty nakažených/vylěčených/úmrtí pro každou zemi včetně požadovaných charakteristik
+﻿Tabulka obsahuje časově závislé hodnoty nakažených/vylěčených/úmrtí pro každou zemi
+včetně požadovaných charakteristik
  pro dané země (viz níže).
 
 
 
-(*)Hodnoty pro sloupce mortality_under5, GDP_population a gini odpovídají posledním uvedeným hodnotám
+(*) Hodnoty pro sloupce MORTALITY_5, GDP_population a GINI odpovídají 
+posledním dostupným hodnotám od roku 2015
 
 
 
@@ -15,22 +17,23 @@ Popis sloupců
 
 ==============
 
-* WEEKEND: Ke každému datu je přiřazena binární hodnota pokud jde o pracovní den (0) nebo víkend (1).
+WEEKEND: Ke každému datu je přiřazena binární hodnota pokud jde o pracovní den (0) nebo víkend (1)
+SEASON: 0-zimní měsíce, 1 - jarní měsíce, 2 - letní měsíce, 3 - podzimní měsíce
 
-* SEASON: 0-zimní měsíce, 1 - jarní měsíce, 2 - letní měsíce, 3 - podzimní měsíce
-
-* POPULATION: celková populace dané země
-* POP_DENSITY_CALCULTED: vypočtená hustota zalidnění
-
-* GDP_POPULATION: HDP na obyvatele (*)
-* GINI: gini koeficient (*)
-* MEDIAN_AGE_2018: medián věku obyvatel (*)
-* REGION_IN_WORLD: světový region dané země
-* LIFE_EXP_DIFF: rozdíl mezi očekávanou dobou dožití v roce 1965 a v roce 2015
-
-
-náboženství rok 2020
-
-RAIN_HOURS: počet hodin, kdy byly srážky nenulové 
-GUST_MAX: maximální síla větru v nárazech během dne
+CUMULATIVE: celkový počet provedených testů
+TESTS_PERFORMED: počet testů v daném dni
+LIFE_EXP_DIFF: rozdíl mezi očekávanou dobou dožití v roce 1965 a v roce 2015
+MEDIAN_AGE_2018: medián věku obyvatel 
+POP_DENSITY_CALCULTED: vypočtená hustota zalidnění
+POPULATION: celková populace dané země
+REGION_IN_WORLD: světový region dané země
+GDP_POPULATION: HDP na obyvatele (*)
+GINI: gini koeficient (*)
+MORTALITY_5 : dětská úmrtnost (*)
 AVG_TEMP: průměrná denní teplota (spočteno z údajů pro 6AM - 6PM v tříhodinových intervalech)
+GUST_MAX: maximální síla větru v nárazech během dne
+RAIN_HOURS: počet hodin, kdy byly srážky nenulové 
+CHRISTIANITY, ISLAM, BUDDHISM, HINDUISM,
+	JUDAISM, FOLK RELIGIONS, UNAFFILIATED RELIGIONS, 
+	OTHER RELIGIONS - procentuální podíl náboženství, data pro rok 2020
+
--- Projekty/SQL_projekt.sql
+++ Projekty/SQL_projekt.sql
@@ -1,30 +1,40 @@
 
---- FINAL -----
+-- FINAL -----
+update t_barbora_kacerovska_SQL_covid 
+set country = 'Czech Republic'
+where country = 'Czechia'
 
-SELECT c.*, 
+create or replace table t_barbora_kacerovska_projekt_SQL_final as;
+SELECT c.*,
+	ct.cumulative, ct.tests_performed,
 		le.LIFE_EXP_DIFF, le.median_age_2018, le.POP_DENSITY_CALCULATED, le.population, le.region_in_world, le.surface_area,
 		e.GDP_population, e.GINI, e.MORTALITY_5,
-		w.AVG_TEMP, w.GUST_MAX, w.RAIN_HOURS 
+		w.AVG_TEMP, w.GUST_MAX, w.RAIN_HOURS,
+		r.Christianity, r.Islam, r.Buddhism, r.Hinduism, r.Judaism, r.`Folk religions`, r.`Unaffiliated Religions`, r.`Other Religions` 
 from t_barbora_kacerovska_SQL_covid c
+left join (select iso, date, cumulative, tests_performed from covid19_tests ct) ct on c.iso3 = ct.iso and c.date = ct.date
 left join t_barbora_kacerovska_SQL_LE le on c.iso3 = le.iso3 
 left join t_barbora_kacerovska_SQL_economy e on c.country = e.country
 left join t_barbora_kacerovska_SQL_weather w on c.iso3 = w.iso3 and c.date = w.date
 left join t_barbora_kacerovska_SQL_religion r on c.country = r.country;
 
 
-
-
---- COVID DATA ----- 
-create or replace table t_barbora_kacerovska_SQL_covid as
+-- COVID DATA
+create or replace table t_barbora_kacerovska_SQL_covid as;
 SELECT 
 	cb.*,
-	case when weekday(date) in (5,6) then 1 else 0 end as weekend,  # 5=sobota,  6=nedele
+	lt.iso3,
+	case when weekday(date) in (5,6) then 1 else 0 end as WEEKEND,  # 5=sobota,  6=nedele
 	case when month(date) in (12, 1, 2) then 0
       when month(date) in (3, 4, 5) then 1
       when month(date) in (6, 7, 8) then 2
       when month(date) in (9, 10, 11) then 3 # mesice
 	  end as SEASON
-FROM (select * from covid19_basic cb where date='2020-03-03') cb;   #doèasnì, **********ZRUŠIT
+FROM covid19_basic cb
+left join (select distinct iso3, country from lookup_table lt where province is null) lt on cb.country = lt.country;  
+
+
+
 
 
 -- LIFE EXPECTANCY + BASIC INFO----- 
@@ -40,15 +50,15 @@ left join (
 left JOIN (
 	select iso3, median_age_2018, region_in_world, population, surface_area
 	from countries c 
-	where iso3 is not null) c 
+	where iso3 is not null and country != 'Northern Ireland') c 
 	on le1.iso3 = c.iso3 
 where le1.iso3 is not null
 
-	
---- RELIGION ----------
+
+-- RELIGION ---
 
 
-create or replace table t_barbora_kacerovska_SQL_religion as
+create or replace table t_barbora_kacerovska_SQL_religion as;
 select r2.country, 
 	sum(case when r3.religion='Christianity'  then round(r3.population/r2.pop_total*100,2)  else 0 end) as 'Christianity',
 	sum(case when r3.religion='Islam'  then round(r3.population/r2.pop_total*100,2)  else 0 end) as 'Islam',
@@ -58,11 +68,12 @@ select r2.country,
 	sum(case when r3.religion='Folk religions'  then round(r3.population/r2.pop_total*100,2)  else 0 end) as 'Folk religions',
 	sum(case when r3.religion='Judaism'  then round(r3.population/r2.pop_total*100,2)  else 0 end) as 'Judaism',
 	sum(case when r3.religion='Other Religions'  then round(r3.population/r2.pop_total*100,2)  else 0 end) as 'Other Religions'
-from (
+from(
 	select country, sum(population) as pop_total
 	from religions r2
-	where year=2020
-	group by country) r2
+	where year = 2020 and country != 'All Countries'
+	group by country
+	having sum(population)!=0) r2
 left join (
 	select religion, country, population
 	from religions r3 
@@ -70,9 +81,10 @@ left join (
 	on r2.country=r3.country
 group by r2.country;	
 
+
 --- --WEATHER-------------
 
-create or replace table t_barbora_kacerovska_SQL_weather as;
+create or replace table t_barbora_kacerovska_SQL_weather as
 select capital_city, iso3, rain.date, rain.GUST_MAX, rain.RAIN_HOURS, temp.AVG_TEMP
 from countries c
 join (
@@ -87,6 +99,7 @@ left join (
 	group by w.city, w.date) temp
 	on capital_city = temp.city
  	and rain.date = temp.date
+
 
 
 --- ECONOMY --------
@@ -108,5 +121,4 @@ LEFT JOIN(
 	on e2.country = mort.country
 where GDP is not null and `year`>2015
 group by e2.country
-
 
